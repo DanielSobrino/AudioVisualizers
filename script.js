2a26ad643f936264af826459b35ec46188dbe576
DanielSobrino <dsobrino343@gmail.com>
2022-06-01 13:07:51 +0200

    Colorful audio visualiser

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileUpload');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
@@ -16,6 +17,37 @@ container.addEventListener('click', function () {
     analyser = audioCtx.createAnalyser();
     audioSource.connect(analyser);
     analyser.connect(audioCtx.destination);
+    analyser.fftSize = 128; // 64 => 32 bufferLength (32 bars)
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    const barWidth = canvas.width / bufferLength;
+    let barHeight;
+    let x;
+
+    function animate() {
+        x = 0;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        analyser.getByteFrequencyData(dataArray);
+
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+
+        requestAnimationFrame(animate);
+    }
+    animate();
+});
+
+file.addEventListener('change', function () {
+    const files = this.files;
+    const audio1 = document.getElementById('audio1');
+    audio1.src = URL.createObjectURL(files[0]);
+    audio1.load();
+    audio1.play();
+
+    audioSource = audioCtx.createMediaElementSource(audio1);
+    analyser = audioCtx.createAnalyser();
+    audioSource.connect(analyser);
+    analyser.connect(audioCtx.destination);
     analyser.fftSize = 64; // 64 => 32 bufferLength (32 bars)
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
@@ -28,14 +60,22 @@ container.addEventListener('click', function () {
         x = 0;
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         analyser.getByteFrequencyData(dataArray);
-        // draw
-        for (let i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i];
-            ctx.fillStyle = 'white';
-            ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-            x += barWidth;
-        }
+
+        drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray);
+
         requestAnimationFrame(animate);
     }
     animate();
 });
+
+function drawVisualiser(bufferLength, x, barWidth, barHeight, dataArray) {
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        const red = (i * barHeight) / 10;
+        const green = i * 6;
+        const blue = barHeight / 3;
+        ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+}
